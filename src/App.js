2dab7e7018c6b35eaf3e482d230c3c5d4dee362d
gordonwoodbine <gordon_woodbine@yahoo.co.uk>
2021-04-23 16:31:12 +0100

    -probably breaking stuff

--- src/App.js
+++ src/App.js
@@ -10,22 +10,22 @@ function App() {
     <Router>
       <div className="App">
         <Navbar />
-      <div className="content">
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/create">
-            <Create />
-          </Route>
-          <Route path="/blogs/:id">
-            <BlogDetails />
-          </Route>
-          <Route path="*">
-            <NotFound />
-          </Route>
-        </Switch>
-      </div>
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </div>
       </div>
     </Router>
   );
